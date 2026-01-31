91bc51b84152a8ec8851bc416c03b7db63bcbbab
Rkoester47 <koestr@rpi.edu>
2026-01-31 17:43:25 -0500

    Cleaned up clutter and bloat in skeleton

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
 import '../css/App.css'
 
 function Home() {
@@ -10,15 +8,7 @@ function Home() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + Justin</h1>
+      <h1>CAPY</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,13 +16,9 @@ function Home() {
         <button onClick={() => navigate('/dashboard')}>
           Navigate to Dashboard
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
