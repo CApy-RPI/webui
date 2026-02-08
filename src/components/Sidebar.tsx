f0f89d377ca5ac6387df96a3d77969bbb41b30e3
JManion32 <jmanion32@gmail.com>
2026-02-07 19:44:53 -0500

    Polished sidebar, Added Hero Icons, Grayscaled the UI
    Sidebar animation and sizing is clean and polished, Added Hero Icons, Grayscaled the UI, no more colors

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,10 @@
 import '../css/sidebar.css';
 import SidebarLink from './SidebarLink.tsx';
 
+// Icon Imports
+import { Bars3Icon, HomeIcon, StarIcon, CalendarDaysIcon, UserGroupIcon, BellAlertIcon, UserCircleIcon, Cog8ToothIcon } from "@heroicons/react/24/solid";
+
+
 type SidebarProps = {
     collapsed: boolean;
     onToggle: () => void;
@@ -10,18 +14,21 @@ export default function Sidebar({ collapsed, onToggle }: SidebarProps) {
     return (
         <div className={`sidebar ${collapsed ? 'collapsed' : ''}`}>
             <div className="sidebar-header">
+                <div className="toggle-spacer" />
                 <button className="collapse-toggle" onClick={onToggle}>
-                    {collapsed ? 'O' : 'Collapse'}
+                    <Bars3Icon/>
                 </button>
             </div>
             <div className="main-sidebar-link-container">
-                <SidebarLink label="Dashboard" icon="1" nav="/dashboard" />
-                <SidebarLink label="Events" icon="2" nav="/events" />
-                <SidebarLink label="Notifications" icon="3" nav="/notifications" />
+                <SidebarLink label="Dashboard" icon={<HomeIcon />} nav="/dashboard" />
+                <SidebarLink label="Events" icon={<StarIcon />} nav="/events" />
+                <SidebarLink label="Calendar" icon={<CalendarDaysIcon />} nav="/calendar" />
+                <SidebarLink label="Roster" icon={<UserGroupIcon />} nav="/roster" />
+                <SidebarLink label="Notifications" icon={<BellAlertIcon />} nav="/notifications" />
             </div>
             <div className="lower-sidebar-link-container">
-                <SidebarLink label="My Profile" icon="1" nav="/profile" />
-                <SidebarLink label="Home" icon="0" nav="/" />
+                <SidebarLink label="My Profile" icon={<UserCircleIcon />} nav="/profile" />
+                <SidebarLink label="Settings" icon={<Cog8ToothIcon />} nav="/settings" />
             </div>
         </div>
     );
