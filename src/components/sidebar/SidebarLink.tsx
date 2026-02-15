db7f2a304a870b9f89cd718cdbfd45960f1cfaf6
JManion32 <jmanion32@gmail.com>
2026-02-15 15:22:14 -0500

    Polished Sidebar

--- src/components/sidebar/SidebarLink.tsx
+++ src/components/sidebar/SidebarLink.tsx
@@ -2,18 +2,18 @@ import { NavLink } from 'react-router-dom';
 
 type SidebarLinkProps = {
     label: string;
-    icon: React.ReactNode; // TODO: Fix this
+    icon: React.ReactNode;
     nav: string;
 };
 
-export default function SidebarLink({ label, icon, nav }: SidebarLinkProps) {
+export default function SidebarLink({ label, icon, nav}: SidebarLinkProps) {
     return (
         <NavLink
             to={nav}
             className={({ isActive }) => `sidebar-link ${isActive ? 'active-link' : ''}`}
         >
             <span className="sidebar-link-icon">{icon}</span>
-            <span className="sidebar-link-label">{label}</span>
+            <span className="sidebar-link-label sidebar-label-transition">{label}</span>
         </NavLink>
     );
 }
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,8 +1,11 @@
+import DarkModeToggle from '../components/DarkModeToggle'
+
 export default function Settings() {
     return (
         <>
             <div>
                 <h1>Settings</h1>
+                <DarkModeToggle/>
             </div>
         </>
     );
