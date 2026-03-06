1b43a1c1f48c4bd5ce9d943afe1379930b28a45a
JManion32 <jmanion32@gmail.com>
2026-03-05 23:54:50 -0500

    Linter

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -3,13 +3,12 @@ import { Outlet } from 'react-router-dom';
 import Navbar from './navbar/Navbar';
 
 export default function AppLayout() {
-
-  return (
-    <div className="main-app-container">
-      <Navbar />
-      <div className="app-content">
-            <Outlet />
-      </div>
-    </div>
-  );
+    return (
+        <div className="main-app-container">
+            <Navbar />
+            <div className="app-content">
+                <Outlet />
+            </div>
+        </div>
+    );
 }
