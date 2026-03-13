c248f3d2a32a4542a6d2118a5d273fed67ea5adc
Rkoester47 <184577759+Rkoester47@users.noreply.github.com>
2026-03-12 20:43:36 -0400

    Prettier fixes

--- src/pages/Testing.tsx
+++ src/pages/Testing.tsx
@@ -1,4 +1,4 @@
-import DarkModeToggle from "../components/DarkModeToggle";
+import DarkModeToggle from '../components/DarkModeToggle';
 
 export default function Testing() {
     return (
@@ -6,7 +6,7 @@ export default function Testing() {
             <div style={{ display: 'flex', flexDirection: 'column', gap: '16px', color: 'black' }}>
                 <h1>[dev] Testing</h1>
                 <p>This page is for misc features that have been developed, but not placed.</p>
-                <DarkModeToggle/>
+                <DarkModeToggle />
             </div>
         </>
     );
