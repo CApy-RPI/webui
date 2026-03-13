0eb5d63487db25b55f8e917d1314fdfe969d5b8f
Rkoester47 <184577759+Rkoester47@users.noreply.github.com>
2026-03-12 20:39:03 -0400

    Moved Light Mode Toggle back into Testing
    Kept stylistic changes for light mode, but put it back into Testing for now.

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import Events from './pages/Events.tsx';
 import Organizations from './pages/Organizations.tsx';
 import Testing from './pages/Testing.tsx'; // temp page
 import Profile from './pages/Profile.tsx';
-import DarkModeToggle from './components/DarkModeToggle';
 import { AuthProvider } from './contexts/AuthContext.tsx';
 
 export default function App() {
@@ -24,7 +23,6 @@ export default function App() {
                                 <Route path="/testing" element={<Testing />} />
                             </Route>
                         </Routes>
-                        <DarkModeToggle />
                     </BrowserRouter>
                 </ThemeProvider>
             </AuthProvider>
